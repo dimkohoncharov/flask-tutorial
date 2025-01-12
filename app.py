f938e22ac24b29537f40f9c4b8510dddd8efc95c
dimkohoncharov <honcharov.dimko@gmail.com>
2025-01-12 17:09:27 +0200

    add login page via wtforms

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 """Flask Application for Paws Rescue Center."""
 from flask import Flask, render_template, abort, request
+from forms import LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
 
 """Information regarding the Pets in the System."""
 pets = [
@@ -45,6 +48,19 @@ def simplelogin():
         else:
             return render_template("simplelogin.html", message="Incorrect Email or Password!")
     return render_template("simplelogin.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        for u_email, u_password in users.items():
+            if u_email == form.email.data and u_password == form.password.data:
+                return render_template("login.html", message="Successfully logged in!")
+        return render_template("login.html", message="Incorrect Email or Password!")
+    elif form.errors:
+        print(form.errors.items())
+    return render_template("login.html", form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
