639b84b8cef71304eb5dc5a2147cb1add1a937a2
dimkohoncharov <honcharov.dimko@gmail.com>
2025-02-10 20:26:35 +0200

    Add basic connection to db, flash messages

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 """Flask Application for Paws Rescue Center."""
-from flask import Flask, render_template, abort, request
+from flask import Flask, render_template, abort, request, flash
 from forms import LoginForm, SignupForm
 from flask import session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///example.db"
+db = SQLAlchemy(app) 
+
+"""Model for Pets."""
+class Pet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    age = db.Column(db.String)
+    bio = db.Column(db.String)
+
+"""Model for Users."""
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+with app.app_context():
+    db.create_all()
 
 """Information regarding the Pets in the System."""
 pets = [
@@ -15,11 +35,9 @@ pets = [
         ]
 
 """Information regarding the Users in the System"""
-users = {
-    "archie.andrews@email.com": "football4life",
-    "veronica.lodge@email.com": "fashiondiva"
-}
-
+users = [
+            {"id": 1, "full_name": "Pet Rescue Team", "email": "team@pawsrescue.co", "password": "adminpass"},
+        ]
 @app.route("/")
 def homepage():
     """View function for Home Page."""
@@ -59,7 +77,9 @@ def login():
             return render_template("login.html", form = form, message = "Wrong Credentials. Please Try Again.")
         else:
             session['user'] = user
-            return render_template("login.html", message = "Successfully Logged In!")
+            flash("Successfully Logged In!")
+            return redirect(url_for("homepage", _scheme="http", _external=True, message="Successfully Logged In!"))
+            # return render_template("login.html", message = "Successfully Logged In!")
 
     return render_template("login.html", form=form)
 
@@ -73,9 +93,11 @@ def logout():
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
-        new_user = {"id": len(users)+1, "email": form.email.data}
+        new_user = {"id": len(users)+1, "email": form.email.data, "password": form.password.data}
+        users.append(new_user)
         session['user'] = new_user
-        return render_template("signup.html", message="Succcessfully signed up!")
+        flash("Successfully signed in!")
+        return redirect(url_for("homepage", _scheme="http", _external=True))
     return render_template("signup.html", form=form)
 
 if __name__ == "__main__":
